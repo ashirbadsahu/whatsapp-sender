8a13d3f646ca5d5456346ddf6a58f48855c8f017
ashirbadsahu <ashirbadsahu300@gmail.com>
2023-10-02 15:45:19 +0530

    added readme.md

---
+++ README.md
@@ -0,0 +1,8 @@
+# Automatic Whatsapp message sender
+This is a python bot to send whatsapp messages to numbers at a specified time(24hrs format local time).
+
+Install a dependency named **pywhatkit** to use this.
+
+```
+pip install pywhatkit
+```
--- sender.py
+++ sender.py
@@ -1,12 +1,12 @@
 import pywhatkit
 
 # Define the message you want to send
-message = "Hii Ashish!"
+message = "Hii there!"
 
 phone_numbers = ["+919999999999", "+918888888888"]
 
 # Send the message to each phone number in the list
 for phone_number in phone_numbers:
-    pywhatkit.sendwhatmsg(phone_number, message, 19, 36)
+    pywhatkit.sendwhatmsg(phone_number, message, 19, 48)
 
 print("Messages sent successfully!")
